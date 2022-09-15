98501cbfa8088e089b3de5e40dead7b7e0e4b5e7
Konstantin Pingol <constantinep@mail.ru>
2022-09-15 21:08:43 +0300

    Initial commit

---
+++ src/main/java/com/epam/jwd/audiotrack_ordering/Application.java
@@ -0,0 +1,13 @@
+package com.epam.jwd.audiotrack_ordering;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Application {
+
+    private static final Logger LOG = LogManager.getLogger(Application.class);
+    public static void main(String[] args) {
+        LOG.trace("Program start");
+
+    }
+}
